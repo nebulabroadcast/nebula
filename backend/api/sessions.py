82f7a487c49a21104398c7185f5826915b7bf84d
Martastain <martas@imm.cz>
2023-05-05 22:25:40 +0200

    session list

--- backend/api/sessions.py
+++ backend/api/sessions.py
@@ -4,6 +4,10 @@ from fastapi import Query
 from server.session import SessionModel, Session
 from server.dependencies import CurrentUser
 from server.request import APIRequest
+from server.models import RequestModel
+
+class SessionsRequest(RequestModel):
+    id_user: int = Query(..., example=1)
 
 
 class Sessions(APIRequest):
@@ -13,10 +17,12 @@ class Sessions(APIRequest):
 
     async def handle(
         self,
+        request: SessionsRequest,
         user: CurrentUser,
-        id_user: int | None = Query(None)
     ) -> list[SessionModel]:
         """Create or update an object."""
+
+        id_user = request.id_user
 
         if id_user != user.id and (not user.is_admin):
             raise nebula.ForbiddenException()
