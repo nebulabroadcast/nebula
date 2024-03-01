ce068463071e6c458f4e5b83edd64975e7cc8ac2
Martastain <martas@imm.cz>
2024-03-01 23:14:36 +0100

    migrated cli, api and solver plugins to plugin_library

--- backend/nebula/plugins/__init__.py
+++ backend/nebula/plugins/__init__.py
@@ -1,10 +1,10 @@
 __all__ = ["SolverPlugin", "CLIPlugin"]
 
 from .common import modules_root
+
+from .cli import CLIPlugin
 from .solver import SolverPlugin
 
 assert modules_root
 
 
-class CLIPlugin:
-    name: str = "cli_plugin"
