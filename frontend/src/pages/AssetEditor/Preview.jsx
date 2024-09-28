e6509af9d81eed2cc8b23951b65310018c2d49b3
Martastain <martas@imm.cz>
2024-09-28 13:32:19 +0200

    refactor: preview page clean-up

--- frontend/src/pages/AssetEditor/Preview.jsx
+++ frontend/src/pages/AssetEditor/Preview.jsx
@@ -16,11 +16,52 @@ import VideoPlayer from '/src/containers/VideoPlayer'
 import Subclip from './Subclip'
 import { useKeyDown } from '/src/hooks'
 
+const SubclipsPanel = ({ subclips, setSubclips, selection, setSelection }) => {
+  return (
+    <section className="grow">
+      <div
+        className="contained column"
+        style={{
+          overflowY: 'scroll',
+          display: 'flex',
+          gap: 8,
+          justifyContent: 'flex-start',
+          padding: 6,
+        }}
+      >
+        {subclips.map((subclip, index) => (
+          <Subclip
+            key={index}
+            index={index}
+            setSubclips={setSubclips}
+            selection={selection}
+            setSelection={setSelection}
+            {...subclip}
+          />
+        ))}
+        <Spacer />
+      </div>
+    </section>
+  )
+}
+
 const Preview = ({ assetData, setAssetData }) => {
   const accessToken = nebula.getAccessToken()
   const [selection, setSelection] = useState({})
   const [subclips, setSubclips] = useState([])
   const [position, setPosition] = useState(0)
+
+  // Video source
+
+  const videoSrc =
+    assetData.id && accessToken && `/proxy/${assetData.id}?token=${accessToken}`
+
+  const patchAsset = (data) => {
+    // helper function to update asset data
+    setAssetData((o) => {
+      return { ...o, ...data }
+    })
+  }
 
   useEffect(() => {
     setSelection({})
@@ -28,54 +69,40 @@ const Preview = ({ assetData, setAssetData }) => {
   }, [assetData?.id])
 
   useEffect(() => {
+    // when subclip list changes, update it in asset data
     if (!assetData) return
     if ((assetData.subclips || []) !== subclips) {
-      setAssetData((assetData) => ({
-        ...assetData,
-        subclips: subclips.length ? subclips : null,
-      }))
+      patchAsset({ subclips: subclips.length ? subclips : null })
     }
   }, [subclips])
 
-  const onSetMarks = () => {
-    setAssetData((o) => {
-      return {
-        ...o,
-        mark_in: selection.mark_in || null,
-        mark_out: selection.mark_out || null,
-      }
-    })
-  }
-
-  const videoSrc =
-    assetData.id && accessToken && `/proxy/${assetData.id}?token=${accessToken}`
+  // Dropdown menu options for poster frame
 
   const posterOptions = [
     {
       label: 'Set poster frame',
-      onClick: () => {
-        setAssetData((o) => {
-          return { ...o, poster_frame: position }
-        })
-      },
+      onClick: () => patchAsset({ poster_frame: position }),
     },
     {
       label: 'Go to poster frame',
-      onClick: () => {
-        setPosition(assetData.poster_frame)
-      },
+      onClick: () => setPosition(assetData.poster_frame),
     },
     {
       label: 'Clear poster frame',
-      onClick: () => {
-        setAssetData((o) => {
-          return { ...o, poster_frame: null }
-        })
-      },
+      onClick: () => patchAsset({ poster_frame: null }),
     },
   ]
 
   // Actions
+
+  const onSetMarks = () => {
+    // Set asset mark_in and mark_out values
+    // (content primary selection)
+    patchAsset({
+      mark_in: selection.mark_in || null,
+      mark_out: selection.mark_out || null,
+    })
+  }
 
   const onNewSubclip = () => {
     if (!(selection.mark_in && selection.mark_out)) {
@@ -94,36 +121,9 @@ const Preview = ({ assetData, setAssetData }) => {
     ])
   }
 
+  // Keyboard shortcuts
+
   useKeyDown('v', onNewSubclip)
-
-  /*
-  const onNewSubclip = useCallback(() => {
-    if (!(selection.mark_in && selection.mark_out)) {
-      toast.error('Please select a region first')
-      return
-    }
-
-    if (selection.mark_in >= selection.mark_out) {
-      toast.error('Please select a valid region')
-      return
-    }
-
-    setSubclips((subclips) => [
-      ...subclips,
-      { title: `SubClip ${subclips.length + 1}`, ...selection },
-    ])
-  }, [selection])
-
-  useEffect(() => {
-    const handleKeyDown = (e) => {
-      if (e.key === 'v') {
-        onNewSubclip()
-      }
-    }
-    window.addEventListener('keydown', handleKeyDown)
-    return () => window.removeEventListener('keydown', handleKeyDown)
-  }, [onNewSubclip])
-   */
 
   // Render
 
@@ -176,30 +176,12 @@ const Preview = ({ assetData, setAssetData }) => {
           <Dropdown icon="image" align="right" options={posterOptions} />
         </Navbar>
 
-        <section className="grow">
-          <div
-            className="contained column"
-            style={{
-              overflowY: 'scroll',
-              display: 'flex',
-              gap: 8,
-              justifyContent: 'flex-start',
-              padding: 6,
-            }}
-          >
-            {subclips.map((subclip, index) => (
-              <Subclip
-                key={index}
-                index={index}
-                setSubclips={setSubclips}
-                selection={selection}
-                setSelection={setSelection}
-                {...subclip}
-              />
-            ))}
-            <Spacer />
-          </div>
-        </section>
+        <SubclipsPanel
+          subclips={subclips}
+          setSubclips={setSubclips}
+          selection={selection}
+          setSelection={setSelection}
+        />
       </div>
     </div>
   )
