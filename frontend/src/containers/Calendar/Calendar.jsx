a6123903834dc245960df5953669fb7cde71955c
Martastain <martas@imm.cz>
2024-10-06 19:33:39 +0200

    feat: display time markers in calendar

--- frontend/src/containers/Calendar/Calendar.jsx
+++ frontend/src/containers/Calendar/Calendar.jsx
@@ -7,7 +7,7 @@ const CalendarCanvas = styled.canvas`
   background-color: #24202e;
 `
 
-const CLOCK_WIDTH = 100
+const CLOCK_WIDTH = 40
 
 const Calendar = ({ startTime }) => {
   const calendarRef = useRef(null)
@@ -74,20 +74,35 @@ const Calendar = ({ startTime }) => {
       startHour + (startMinute + ((15 - (startMinute % 15)) % 15)) / 60
     )
 
-    for (let i = 0; i < 24 * 4; i++) {
-      const timeMarker = new Date(firstTime.getTime() + i * 15 * 60000)
-      const { x, y } = time2pos(timeMarker)
+    for (let rday = 0; rday < 7; rday++) {
+      for (let rtime = 0; rtime < 24 * 4; rtime++) {
+        const timeMarker = new Date(firstTime.getTime() + rtime * 15 * 60000)
+        const minutes = timeMarker.getMinutes()
+        const { x, y } = time2pos(timeMarker)
 
-      for (let i = 0; i < 7; i++) {
-        const x1 = x + i * dayWidth + 10
+        if (hourHeight < 50 && minutes !== 0) {
+          continue
+        }
+
+        const x1 = x + rday * dayWidth + 10
         const x2 = x1 + dayWidth - 10
+
         ctx.beginPath()
         ctx.strokeStyle = '#444'
-        ctx.setLineDash([5, 5])
+        if (minutes === 0) {
+          ctx.setLineDash([1, 0])
+        } else {
+          ctx.setLineDash([5, 5])
+        }
         ctx.moveTo(x1, y)
         ctx.lineTo(x2, y)
         ctx.stroke()
         ctx.setLineDash([]) // Reset to solid lines for other drawings
+        if (rday === 0 && minutes === 0) {
+          ctx.font = '12px Arial'
+          ctx.fillStyle = '#fff'
+          ctx.fillText(timeMarker.toTimeString().slice(0, 5), 10, y + 4)
+        }
       }
     }
   }
