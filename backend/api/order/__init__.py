72bd7ce6fc89d1fe11956ed72d5bfe418792e148
Martastain <martas@imm.cz>
2024-10-15 22:49:58 +0200

    refactor: clean-up complex endpoints

--- backend/api/order/__init__.py
+++ backend/api/order/__init__.py
@@ -1,31 +1,3 @@
-import nebula
-from nebula.helpers.scheduling import bin_refresh
-from server.dependencies import CurrentUser, RequestInitiator
-from server.request import APIRequest
+__all__ = ["OrderRequest"]
 
-from .models import OrderRequestModel, OrderResponseModel
-from .order import set_rundown_order
-
-
-class Request(APIRequest):
-    """Set the order of items of a rundown"""
-
-    name = "order"
-    title = "Order"
-    response_model = OrderResponseModel
-
-    async def handle(
-        self,
-        request: OrderRequestModel,
-        user: CurrentUser,
-        initiator: RequestInitiator,
-    ) -> OrderResponseModel:
-        if not user.can("rundown_edit", request.id_channel):
-            raise nebula.ForbiddenException("You are not allowed to edit this rundown")
-
-        result = await set_rundown_order(request, user)
-        nebula.log.info(f"Changed order in bins {result.affected_bins}", user=user.name)
-
-        # Update bin duration
-        await bin_refresh(result.affected_bins, initiator=initiator, user=user)
-        return result
+from .order_request import OrderRequest
--- backend/api/rundown/__init__.py
+++ backend/api/rundown/__init__.py
@@ -1,37 +1,3 @@
-import nebula
-from server.dependencies import CurrentUser
-from server.request import APIRequest
+__all__ = ["RundownRequest"]
 
-from .models import RundownRequestModel, RundownResponseModel
-from .rundown import get_rundown
-
-
-class Request(APIRequest):
-    """Get a rundown for the given day
-
-    Date is specified in YYYY-mm-dd format and it takes in
-    account the channel broadcast start setting (usually 7:00)
-
-    Rundown is a list of items that are scheduled to be broadcasted
-    in the given day. Items are ordered by their start time and contain
-    additional metadata such as scheduled time, actual broadcast time,
-    etc. During the broadcast, the actual time is updated to reflect
-    the real broadcast time.
-
-    Additional non-playable items are also included in the rundown,
-    such as blocks, placeholders, loop markers and so on.
-    """
-
-    name = "rundown"
-    title = "Get rundown"
-    response_model = RundownResponseModel
-
-    async def handle(
-        self,
-        request: RundownRequestModel,
-        user: CurrentUser,
-    ) -> RundownResponseModel:
-        if not user.can("rundown_view", request.id_channel):
-            raise nebula.ForbiddenException("You are not allowed to view this rundown")
-
-        return await get_rundown(request)
+from .rundown_request import RundownRequest
