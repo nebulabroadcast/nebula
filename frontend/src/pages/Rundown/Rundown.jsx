4b7dec58debb66c1e84fc9de84453379f0ea3cbe
Martastain <martas@imm.cz>
2024-10-17 22:43:41 +0200

    feat: rundown table prototype

--- frontend/src/pages/Rundown/Rundown.jsx
+++ frontend/src/pages/Rundown/Rundown.jsx
@@ -1,14 +1,36 @@
 import { useState, useEffect, useMemo } from 'react'
+import nebula from '/src/nebula'
 
 import RundownNav from './RundownNav'
+import RundownTable from './RundownTable'
 
 const Rundown = () => {
   const [startTime, setStartTime] = useState(null)
+  const [rundown, setRundown] = useState(null)
+
+  const onResponse = (response) => {
+    setRundown(response.data.rows)
+  }
+
+  const onError = (error) => {
+    console.log(error.response)
+  }
+
+  useEffect(() => {
+    if (!startTime) return
+    const requestParams = {
+      date: startTime.toISOString().split('T')[0],
+      id_channel: 1,
+    }
+    nebula.request('rundown', requestParams).then(onResponse).catch(onError)
+  }, [startTime])
 
   return (
     <main className="column">
       <RundownNav startTime={startTime} setStartTime={setStartTime} />
-      <section className="grow nopad">{startTime?.toISOString()}</section>
+      <section className="grow nopad">
+        <RundownTable data={rundown} />
+      </section>
     </main>
   )
 }
