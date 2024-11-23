487d6bf4a3eef1877bdb11d66bcbde6bdb6c6b3e
Martastain <martas@imm.cz>
2024-11-23 16:31:02 +0100

    fix: formatMetaDatetimes with null values

--- frontend/src/tableFormat/formatMetaDatetime.jsx
+++ frontend/src/tableFormat/formatMetaDatetime.jsx
@@ -1,9 +1,18 @@
 import { Timestamp } from '/src/components'
 
-const formatMetaDatetime = (rowData, key, mode = 'datetime') => (
-  <td>
-    <Timestamp timestamp={rowData[key]} mode={mode} />
-  </td>
-)
+const formatMetaDatetime = (rowData, key, mode = 'datetime') => {
+  const timestamp = rowData[key]
+  if (!timestamp)
+    return (
+      <td>
+        <hr />
+      </td>
+    )
+  return (
+    <td>
+      <Timestamp timestamp={rowData[key]} mode={mode} />
+    </td>
+  )
+}
 
 export default formatMetaDatetime
