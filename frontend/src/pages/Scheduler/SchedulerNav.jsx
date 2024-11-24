7d5940347eb7813bbcc5fb1f8d3a9bc493ac29e6
Martastain <martas@imm.cz>
2024-11-24 23:16:25 +0100

    fix: use day start properly in calendar

--- frontend/src/pages/Scheduler/SchedulerNav.jsx
+++ frontend/src/pages/Scheduler/SchedulerNav.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useSearchParams } from 'react-router-dom'
 
+import nebula from '/src/nebula'
 import { createTitle } from './utils'
 import { setPageTitle } from '/src/actions'
 
@@ -13,6 +14,10 @@ const SchedulerNav = ({ setStartTime }) => {
   const currentChannel = useSelector((state) => state.context.currentChannel)
   const dispatch = useDispatch()
 
+  const channelConfig = useMemo(() => {
+    return nebula.getPlayoutChannel(currentChannel)
+  }, [currentChannel])
+
   useEffect(() => {
     // When the query param `date` changes, update the start time
     // of the calendar view and the page title
@@ -21,17 +26,19 @@ const SchedulerNav = ({ setStartTime }) => {
     if (date && dateParam === date) return
     if (!dateParam) dateParam = new Date().toISOString().split('T')[0]
 
+    const [dsHH, dsMM] = channelConfig.day_start
+
     const newDate = new Date(dateParam)
     const dayOfWeek = newDate.getDay()
     const diff = newDate.getDate() - dayOfWeek + (dayOfWeek === 0 ? -6 : 1)
     const weekStart = new Date(newDate.setDate(diff))
-    weekStart.setHours(7, 30, 0, 0)
+    weekStart.setHours(dsHH, dsMM, 0, 0)
 
     const pageTitle = createTitle(weekStart)
     dispatch(setPageTitle({ title: pageTitle }))
     setStartTime(weekStart)
     setDate(dateParam)
-  }, [searchParams, currentChannel])
+  }, [searchParams, channelConfig])
 
   useEffect(() => {
     if (date && date !== searchParams.get('date')) {
