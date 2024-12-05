4b0259417200e0df98010c3933d7636aba8bf8f9
Martastain <martas@imm.cz>
2024-12-05 20:50:01 +0100

    feat: add live item to rundow

--- frontend/src/tableFormat/formatRundownSymbol.jsx
+++ frontend/src/tableFormat/formatRundownSymbol.jsx
@@ -15,6 +15,9 @@ const formatRundownSymbol = (rowData) => {
     icon = 'fiber_manual_record'
   } else if (rowData.item_role === 'placeholder') {
     icon = 'expand'
+  } else if (rowData.item_role === 'live') {
+    icon = 'live_tv'
+    style.color = 'red'
   } else if (rowData.item_role === 'lead_in') {
     icon = 'vertical_align_bottom'
   } else if (rowData.item_role === 'lead_out') {
