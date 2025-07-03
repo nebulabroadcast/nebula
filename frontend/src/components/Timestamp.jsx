fe99dc6772410632165034f87e1f7938c770cebe
Martastain <martas@imm.cz>
2025-07-03 20:55:14 +0200

    fix: local time in table cells

--- frontend/src/components/Timestamp.jsx
+++ frontend/src/components/Timestamp.jsx
@@ -19,8 +19,17 @@ const Timestamp = ({ timestamp, mode, ...props }) => {
   if (!timestamp) return <></>;
   const localDateTime = new Date(timestamp * 1000);
 
-  const dateStr = localDateTime.toISOString().split('T')[0];
-  const timeStr = localDateTime.toISOString().split('T')[1].split('.')[0];
+  const yy = localDateTime.getFullYear();
+  const mm = localDateTime.getMonth() + 1; // Months are zero-based
+  const dd = localDateTime.getDate();
+
+  const hh = localDateTime.getHours();
+  const min = localDateTime.getMinutes();
+  const ss = localDateTime.getSeconds();
+
+  const pad = (n) => String(n).padStart(2, '0');
+  const dateStr = `${yy}-${pad(mm)}-${pad(dd)}`;
+  const timeStr = `${hh}:${pad(min)}:${pad(ss)}`;
 
   return (
     <TimestampWrapper {...props}>
