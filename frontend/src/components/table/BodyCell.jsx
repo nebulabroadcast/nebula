726eb6018cff5e577ac1d1ff31acb8d8648d8b79
Martin Wacker <martas@imm.cz>
2025-09-26 16:30:47 +0000

    feat: context menu contextData

--- frontend/src/components/table/BodyCell.jsx
+++ frontend/src/components/table/BodyCell.jsx
@@ -1,5 +1,17 @@
-const BodyCell = ({ rowData, column, cellFormatter }) => {
+
+const BodyCell = ({ rowData, column, cellFormatter, onCellContextMenu }) => {
+  const handleContextMenu = (e) => {
+    if (onCellContextMenu) {
+      e.preventDefault();
+      onCellContextMenu({
+        column: column.name,
+        value: rowData[column.name],
+        rowData,
+        event: e,
+      });
+    }
+  };
   if (cellFormatter) return cellFormatter(rowData, column.name);
-  return <td>{rowData[column.name]}</td>;
+  return <td onContextMenu={handleContextMenu}>{rowData[column.name]}</td>;
 };
 export default BodyCell;
