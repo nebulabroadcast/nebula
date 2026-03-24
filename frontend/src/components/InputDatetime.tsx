3a85d2dec583f370018a0e07236b465158b499e4
Martastain <martas@imm.cz>
2026-03-24 21:17:26 +0100

    fix: date picker dialog title

--- frontend/src/components/InputDatetime.tsx
+++ frontend/src/components/InputDatetime.tsx
@@ -33,7 +33,7 @@ const CalendarDialog = ({ value, onChange, onClose }: CalendarDialogProps) => {
 
   return (
     <DatePickerDialog
-      data-tooltip="Select Date"
+      title="Select Date"
       value={date.toFormat('yyyy-MM-dd')}
       handleCancel={onClose}
       handleConfirm={(newDateString: string) => {
