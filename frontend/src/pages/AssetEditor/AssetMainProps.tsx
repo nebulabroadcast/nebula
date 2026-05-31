a2518ff32d0d4219c294c8fd20383665260d8145
Martastain <martas@imm.cz>
2026-05-31 15:04:15 +0200

    chore: deps update

--- frontend/src/pages/AssetEditor/AssetMainProps.tsx
+++ frontend/src/pages/AssetEditor/AssetMainProps.tsx
@@ -73,7 +73,7 @@ const AssetMainProps: React.FC<AssetMainPropsProps> = ({
           nebula
             .request(endpoint.endpoint, { id_asset: assetData.id })
             .then((response) => {
-              const ct = response.headers['content-type'];
+              const ct = response.headers['content-type'] as string;
               setContextActionResult({
                 contentType: ct ? contentType.parse(ct).type : 'application/json',
                 payload: response.data,
@@ -101,8 +101,8 @@ const AssetMainProps: React.FC<AssetMainPropsProps> = ({
 
   const sendTo = () => {
     showDialog('sendto', 'Send to...', { assets: [assetData.id] })
-      .then(() => {})
-      .catch(() => {});
+      .then(() => { })
+      .catch(() => { });
   };
 
   const assetActions = useMemo((): DropdownOptionProps[] => {
